e669241970ac1234b7893327c1adfb056536fff8
Sonu <vishaltime460@gmail.com>
2025-02-19 15:02:07 +0530

    First commit

---
+++ app/src/main/java/com/example/gitstudy/appUi/Home.kt
@@ -0,0 +1,11 @@
+package com.example.gitstudy.appUi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {
+
+    println("This is first commit")
+
+}
